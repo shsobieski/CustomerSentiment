aea4294ec06db1512cba3240da0588c91fac11e3
William Newton <will4856@gmail.com>
2020-06-29 16:05:13 -0400

    added back functions from old repo

---
+++ clean_lemmatize_token.py
@@ -0,0 +1,13 @@
+def clean_lemmatize_token(tweet):
+    stop_words = set(stopwords.words('english'))
+    cleaned = tweet.translate(str.maketrans('', '', string.punctuation)).lower()
+    tokenized = word_tokenize(cleaned)
+    filtered = [w for w in tokenized if not w in stop_words]
+    lemmatizer = WordNetLemmatizer()
+    lemmatized = []
+    for word in filtered:
+        lemmatized.append(lemmatizer.lemmatize(word))
+    to_remove = ['rt','mention','sxsw','link']
+    lemmatized = [w for w in lemmatized if w not in to_remove]
+    lemmatized = ' '.join(lemmatized)
+    return lemmatized
