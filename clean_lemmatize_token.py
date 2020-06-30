6dc6e11ff571dad54246fa9e03375a9ea683e785
William Newton <will4856@gmail.com>
2020-06-29 20:23:44 -0400

    added docstring to function

--- clean_lemmatize_token.py
+++ clean_lemmatize_token.py
@@ -1,4 +1,6 @@
 def clean_lemmatize_token(tweet):
+    """Function used to automatically remove stop words, twitter_specific words and punctuation, tokenize, lemmatize
+    and join together words into one string before modeling"""
     stop_words = set(stopwords.words('english'))
     cleaned = tweet.translate(str.maketrans('', '', string.punctuation)).lower()
     tokenized = word_tokenize(cleaned)
